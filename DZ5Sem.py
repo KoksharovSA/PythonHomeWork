64d840c546836f84fb73f0c479d840b063c9e8f9
KoksharovSA_LT <koksharovsa@gmail.com>
2023-01-19 00:27:30 +0700

    Homework 5Sem

--- DZ5Sem.py
+++ DZ5Sem.py
@@ -4,7 +4,9 @@
 def task1(text):
     return ' '.join([x for x in text.split() if not 'абв' in x])
 
+
 print(task1('Привет абваваорлпов чстмоыолабв абв мир'))
+
 
 # 2.Создайте программу для игры с конфетами человек против человека.
 # Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
@@ -39,36 +41,92 @@ def task2():
         print(f'Прграмма взяла: {program_number}')
         candy = candy - program_number
 
-#task2()
+
+# task2()
 
 # 3.Создайте программу для игры в ""Крестики-нолики"".
 import random
 
+
 def task3():
     cells = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    
     def field():
         return f'   1   2   3\n' \
-            f' -------------\n' \
-            f'1| {cells[0][0]} | {cells[0][1]} | {cells[0][2]} |\n' \
-            f' -------------\n' \
-            f'2| {cells[1][0]} | {cells[1][1]} | {cells[1][2]} |\n' \
-            f' -------------\n' \
-            f'3| {cells[2][0]} | {cells[2][1]} | {cells[2][2]} |\n' \
-            f' -------------'
+               f' -------------\n' \
+               f'1| {cells[0][0]} | {cells[0][1]} | {cells[0][2]} |\n' \
+               f' -------------\n' \
+               f'2| {cells[1][0]} | {cells[1][1]} | {cells[1][2]} |\n' \
+               f' -------------\n' \
+               f'3| {cells[2][0]} | {cells[2][1]} | {cells[2][2]} |\n' \
+               f' -------------'
+    
     print(field())
-    def is_win():
-        return 0
-    while True:
-        while True:
+    
+    def who_win():
+        winner = cells[0][0] + cells[0][1] + cells[0][2] + '-' + \
+                 cells[1][0] + cells[1][1] + cells[1][2] + '-' + \
+                 cells[2][0] + cells[2][1] + cells[2][2] + '-' + \
+                 cells[0][0] + cells[1][0] + cells[2][0] + '-' + \
+                 cells[0][1] + cells[1][1] + cells[2][1] + '-' + \
+                 cells[0][2] + cells[1][2] + cells[2][2] + '-' + \
+                 cells[0][0] + cells[1][1] + cells[2][2] + '-' + \
+                 cells[0][2] + cells[1][1] + cells[2][0]
+        result = {'XXX' in winner.split('-'): [False, 'Победил X'],
+                  'OOO' in winner.split('-'): [False, 'Победил O'],
+                  'XXX' not in winner.split('-') and 'OOO' not in winner.split('-'): [True, '', winner],
+                  ' ' not in winner: [False, 'Игра окончена']}[True]
+        return result
+    
+    def correct_coordinate(coordinate):
+        if len(gamer_cell) == 2 \
+                and int(gamer_cell[0]) <= 3 \
+                and int(gamer_cell[1]) <= 3 \
+                and cells[int(gamer_cell[0]) - 1][int(gamer_cell[1]) - 1] == ' ':
+            return True
+        else:
+            return False
+    
+    def ai():
+        dict_response = {cells[0][0] == 'X' and cells[0][1] == 'X': '02',
+                         cells[0][1] == 'X' and cells[0][2] == 'X': '00',
+                         cells[0][0] == 'X' and cells[0][2] == 'X': '01',
+                         cells[1][0] == 'X' and cells[1][1] == 'X': '12',
+                         cells[1][1] == 'X' and cells[1][2] == 'X': '10',
+                         cells[1][0] == 'X' and cells[1][2] == 'X': '11',
+                         cells[2][0] == 'X' and cells[2][1] == 'X': '22',
+                         cells[2][1] == 'X' and cells[2][2] == 'X': '20',
+                         cells[2][0] == 'X' and cells[2][2] == 'X': '21',
+                         cells[0][0] == 'X' and cells[1][0] == 'X': '20',
+                         cells[1][0] == 'X' and cells[2][0] == 'X': '00',
+                         cells[0][0] == 'X' and cells[2][0] == 'X': '10',
+                         cells[0][1] == 'X' and cells[1][1] == 'X': '21',
+                         cells[1][1] == 'X' and cells[2][1] == 'X': '10',
+                         cells[0][1] == 'X' and cells[2][1] == 'X': '11',
+                         cells[0][2] == 'X' and cells[1][2] == 'X': '22',
+                         cells[1][2] == 'X' and cells[2][2] == 'X': '02',
+                         cells[0][2] == 'X' and cells[2][2] == 'X': '12',
+                         cells[0][0] == 'X' and cells[1][1] == 'X': '22',
+                         cells[2][2] == 'X' and cells[1][1] == 'X': '00',
+                         cells[0][0] == 'X' and cells[2][2] == 'X': '11',
+                         cells[0][2] == 'X' and cells[1][1] == 'X': '20',
+                         cells[2][0] == 'X' and cells[1][1] == 'X': '02',
+                         cells[2][0] == 'X' and cells[0][2] == 'X': '11'}
+        result = [y for x, y in dict_response.items() if x == True]
+        return result[random.randint(0, len(result)-1)]
+    
+    while who_win()[0]:
+        while who_win()[0]:
             gamer_cell = input('Введите координату ячейки: ')
-            if len(gamer_cell) == 2 \
-                    and int(gamer_cell[0]) <= 3 \
-                    and int(gamer_cell[1]) <= 3 \
-                    and cells[int(gamer_cell[0])-1][int(gamer_cell[1])-1] == ' ':
-                cells[int(gamer_cell[0])-1][int(gamer_cell[1])-1] = 'X'
-                while True:
-                    x = random.randint(0,2)
-                    y = random.randint(0,2)
+            if correct_coordinate(gamer_cell):
+                cells[int(gamer_cell[0]) - 1][int(gamer_cell[1]) - 1] = 'X'
+                while who_win()[0]:
+                    if 'XX' in who_win()[2]:
+                        x = int(ai()[0])
+                        y = int(ai()[1])
+                    else:
+                        x = random.randint(0, 2)
+                        y = random.randint(0, 2)
                     if cells[x][y] == ' ':
                         cells[x][y] = 'O'
                         break
@@ -76,9 +134,38 @@ def task3():
             else:
                 print('Не верная координата ячейки или ячейка занята.')
         print(field())
-        
-    
+        print(who_win()[1])
+
 
 task3()
+
+
 # 4.Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
+def task4(dir_input, dir_output):
+    with open(dir_input, 'r') as data_in:
+        list_input = data_in.readlines()
+        with open(dir_output, 'w') as data_out:
+            for string in [x.replace('\n', '') for x in list_input]:
+                result = ''
+                count = 0
+                for index, element in enumerate(string):
+                    if index == 0:
+                        count += 1
+                    else:
+                        if string[index] == string[index - 1] and index != len(string) - 1:
+                            count += 1
+                        elif index == len(string) - 1:
+                            if string[index] == string[index - 1]:
+                                result += f'{count + 1}{string[index]} '
+                            else:
+                                result += f'{count}{string[index - 1]} '
+                                count = 1
+                                result += f'{count}{string[index]} '
+                        else:
+                            result += f'{count}{string[index - 1]} '
+                            count = 1
+                data_out.write(f'{result}\n')
+
+
+task4('file5SemInput.txt', 'file5SemOutput.txt')
